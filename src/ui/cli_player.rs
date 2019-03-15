da999aec7a05de7b830b9da2ed47c399f60b2663
tubo28 <arsenic28@gmail.com>
2019-03-15 21:16:18 +0900

    CLIプレイヤーを実装

--- src/ui/cli_player.rs
+++ src/ui/cli_player.rs
@@ -1,6 +1,10 @@
 use super::ai::AI;
 use super::field::Field;
 use super::game::Card;
+use std::cmp::Eq;
+use std::io::Write;
+use std::io::{stdin, stdout};
+use std::str::FromStr;
 
 #[derive(Clone)]
 pub struct CLIPlayer {
@@ -8,8 +12,7 @@ pub struct CLIPlayer {
 }
 
 impl CLIPlayer {
-    pub fn new(name: String, _seed: u8) -> CLIPlayer {
-        println!("random seed is not use");
+    pub fn new(name: String) -> CLIPlayer {
         CLIPlayer { name: name }
     }
 }
@@ -19,16 +22,59 @@ impl AI for CLIPlayer {
         self.name.clone()
     }
 
-    fn choose_card(&mut self, _turn: usize, _field: &Field, cards: &Vec<Card>) -> Card {
-        unimplemented!();
+    fn choose_card(&mut self, turn: usize, field: &Field, cards: &Vec<Card>) -> Card {
+        println!("第{}ターン", turn);
+        field.print();
+        println!("あなたのカード: {:?}", cards);
+        print!("出すカードを入力してください: ");
+        stdout().flush().unwrap();
+        read_valid_card_loop(cards)
     }
 
     fn choose_gather_row(
         &mut self,
-        _turn: usize,
-        _choosed_cards: &Vec<Card>,
-        _field: &Field,
+        turn: usize,
+        choosed_cards: &Vec<Card>,
+        field: &Field,
     ) -> usize {
-        unimplemented!();
+        println!("第{}ターン", turn);
+        field.print();
+        println!(
+            "他のプレイヤーが出したカード: {:?}",
+            choosed_cards
+        );
+        print!(
+            "カードを出せる列がありません。回収する列を選んでください: "
+        );
+        stdout().flush().unwrap();
+        read_valid_card_loop(&[0, 1, 2, 3])
     }
+}
+
+/// 標準入力から 1 行読み込む
+fn read_line() -> String {
+    let mut line = String::new();
+    stdin()
+        .read_line(&mut line)
+        .expect("failed to read from stdin");
+    line.trim().chars().collect()
+}
+
+/// 標準入力から T 型の値を 1 行読み込む
+fn read<T: FromStr>() -> Option<T> {
+    let line = read_line();
+    line.parse::<T>().ok()
+}
+
+/// 標準入力から整数を 1 行読み込めるまで読み続ける
+fn read_loop<T: FromStr>() -> T {
+    read().unwrap_or_else(|| read_loop())
+}
+
+/// valids に含まれる値が読み込まれるまで読み続ける
+fn read_valid_card_loop<T: FromStr + Eq>(valids: &[T]) -> T {
+    let card = read_loop();
+    let pos = valids.iter().position(|x| x == &card);
+    pos.map(|_| card)
+        .unwrap_or_else(|| read_valid_card_loop(valids))
 }
