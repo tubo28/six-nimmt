975bbd5ace65976e1ac3ea4a5a9a136762d5f0b9
tubo28 <arsenic28@gmail.com>
2019-03-18 03:18:12 +0900

    シードを変更

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ use six_nimmt::ai::random_ai::RandomAI;
 use six_nimmt::ui::game::GameManager;
 
 fn main() {
-    let seed = 32;
     let mut gm = GameManager::new();
     let players: Vec<Box<AI>> = vec![
         Box::new(CLIPlayer::new("Alpha".to_string())),
-        Box::new(RandomAI::new("Bravo".to_string(), seed)),
-        Box::new(RandomAI::new("Charlie".to_string(), seed)),
-        Box::new(RandomAI::new("Delta".to_string(), seed)),
-        Box::new(RandomAI::new("Echo".to_string(), seed)),
-        Box::new(RandomAI::new("Foxtrot".to_string(), seed)),
+        Box::new(RandomAI::new("Bravo".to_string(), 2)),
+        Box::new(RandomAI::new("Charlie".to_string(), 3)),
+        Box::new(RandomAI::new("Delta".to_string(), 4)),
+        Box::new(RandomAI::new("Echo".to_string(), 5)),
+        Box::new(RandomAI::new("Foxtrot".to_string(), 6)),
     ];
     let cards = (1..=104).collect();
-    gm.initialize(seed, players, cards);
+    gm.initialize(28, players, cards);
     gm.run();
 }
